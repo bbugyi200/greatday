6fd35a9eefcf17ded2d7ad5b341d77cc4b831fd6
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-15 18:38:51 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ SUPPORTED_PYTHON_VERSIONS = [
     (3, 9),
     (3, 10),
 ]
-USE_SCM_VERSION = {"fallback_version": "0.1.0"}
+USE_SCM_VERSION = {"fallback_version": "0.2.0"}
 
 
 ###############################################################################
--- src/greatday/__init__.py
+++ src/greatday/__init__.py
@@ -7,4 +7,4 @@ APP_NAME: Final = "greatday"
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
