f7bfa00915902da4ba8f22e7886f08a1a9fff23f
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-01-17 13:12:59 -0500

    Add inline comment story to `run_start()`

--- src/greatday/_runners.py
+++ src/greatday/_runners.py
@@ -23,6 +23,45 @@ logger = Logger(__name__)
 def run_start(cfg: StartConfig) -> int:
     """Runner for the 'start' subcommand."""
     del cfg
+    # If any Todos exist with a priority of 'C' or higher...
+    # Process them first...
+
+    # How many days (N) has it been since ticklers were checked?
+
+    # If we didn't check ticklers yet today (i.e. N > 0)...
+    # Process last N days of ticklers...
+
+    # If daily backup file already exists...
+    # Ask the user: Can we delete this backup file?
+
+    # Process all @inbox Todos.
+
+    # Clear daily file of all done Todos.
+
+    ### START: Daily Todo Collection
+    # Collect all Todos in daily file.
+    #
+    # Collect all Todos with priority greater than or equal to 'D'.
+    #
+    # Prompt the user for more Todos (using fuzzy matching).
+    ### END: Daily Todo Collection
+
+    # Render / format the daily file using the Todos we collected.
+
+    # Copy the daily file to the backup daily file.
+
+    # Use vimlala to open daily file in vim.
+
+    # Collect Todos from daily file.
+
+    # Run tests on all collected daily file Todos.
+
+    # For each failed test...
+    # Prompt the user for a fix.
+
+    # Ask the user: Can we commit / save the daily file?
+    # Copy the contents of the backup daily file to the daily file.
+    # Delete the backup daily file.
     return 0
 
 
