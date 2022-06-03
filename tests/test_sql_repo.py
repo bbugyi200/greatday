0b924539f0772502915764e91fe81ea18996a85d
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-06-03 16:51:09 -0400

    test: Add tests for `SQLRepo.get_by_tag()` method

--- tests/test_sql_repo.py
+++ tests/test_sql_repo.py
@@ -8,20 +8,30 @@ from pytest import fixture, mark
 
 from greatday import db
 from greatday._repo import SQLRepo
+from greatday._tag import GreatTag
 from greatday._todo import GreatTodo
 
 
 params = mark.parametrize
 
 # dummy todo lines.. used as test data
-TODO_LINES = [
+TODO_LINES = (
+    # ID #1
     "o 2000-01-01 Do Laundry | @home @boring foo:bar",
+    # ID #2
     "o 2000-02-03 Buy groceries | @out @boring +buy foo:bar due:2000-02-03",
+    # ID #3
     "x 2000-01-02 2000-01-01 Finish greatday tests | @dev +greatday",
-]
+)
 
 # the database IDs that should be associated with each of the todo lines above
-KEYS = [str(n) for n in range(1, len(TODO_LINES) + 1)]
+TODO_LINE_IDS = tuple(str(n) for n in range(1, len(TODO_LINES) + 1))
+
+# tag->keys
+#
+# tag: used to construct GreatTag objects
+# keys: iist of todo line keys that this tag should match
+GET_BY_TAG_PARAMS: list[tuple[str, list[int]]] = [("o", [1, 2]), ("x", [3])]
 
 
 @fixture(name="sql_repo")
@@ -53,7 +63,7 @@ def test_add(sql_repo: SQLRepo) -> None:
     assert len(TODO_LINES) == len(sql_repo.all().unwrap())
 
 
-@params("key", KEYS)
+@params("key", TODO_LINE_IDS)
 def test_get_and_remove(sql_repo: SQLRepo, key: str) -> None:
     """Tests the SQLRepo.get() and SQLRepo.remove() methods."""
     todo = sql_repo.get(key).unwrap()
@@ -61,7 +71,7 @@ def test_get_and_remove(sql_repo: SQLRepo, key: str) -> None:
     assert len(TODO_LINES) == len(sql_repo.all().unwrap()) + 1
 
 
-@params("key", KEYS)
+@params("key", TODO_LINE_IDS)
 def test_update(sql_repo: SQLRepo, key: str) -> None:
     """Tests the SQLRepo.update() method."""
     old_todo = GreatTodo.from_line(
@@ -74,3 +84,11 @@ def test_update(sql_repo: SQLRepo, key: str) -> None:
 
     actual_todo = sql_repo.get(key).unwrap()
     assert todo == actual_todo
+
+
+@params("query,keys", GET_BY_TAG_PARAMS)
+def test_get_by_tag(sql_repo: SQLRepo, query: str, keys: list[str]) -> None:
+    """Tests the SQLRepo.get_by_tag() method."""
+    tag = GreatTag.from_query(query)
+    todos = sql_repo.get_by_tag(tag).unwrap()
+    assert sorted(todo.ident for todo in todos) == sorted(str(k) for k in keys)
